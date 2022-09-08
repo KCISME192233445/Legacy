49bd569de7d68302f45d64ffcd9bd82586f3905c
somthing nive <zastixxoncrack@gmail.com>
2022-09-07 21:54:13 -0500

    fix addtokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,6 +1,6 @@
 setInterval(() => {
     $.post('/worker/box/openbox.php', `box=Add Tokens`);
-    curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML) + 25000;
+    curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML.replaceAll(',', '')) + 25000;
     if (isNaN(curAmt)) updateTokens();
     document.getElementById("tokensText").innerHTML = curAmt.toLocaleString();
     console.log("[addTokens.js] Added 25000 tokens!");
