bc0b7aa36c4460fd698e3866be9072a7f7702578
somthing nive <zastixxoncrack@gmail.com>
2022-09-07 21:47:52 -0500

    made adding tokens to the tokens text client-side

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,5 +1,7 @@
 setInterval(() => {
     $.post('/worker/box/openbox.php', `box=Add Tokens`);
-    updateTokens();
-    console.log("[addTokens.js] Added -25000 tokens!");
-}, 751);
+    curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML) + 25000;
+    if (isNaN(curAmt)) updateTokens();
+    document.getElementById("tokensText").innerHTML = curAmt.toString();
+    console.log("[addTokens.js] Added 25000 tokens!");
+}, 750);
