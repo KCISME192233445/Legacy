eb388fd8e6854cf2b02b79db320d706e49f050d0
somthing nive <zastixxoncrack@gmail.com>
2022-09-07 21:52:19 -0500

    commas on addtokens

--- global/addTokens.js
+++ global/addTokens.js
@@ -2,6 +2,6 @@ setInterval(() => {
     $.post('/worker/box/openbox.php', `box=Add Tokens`);
     curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML) + 25000;
     if (isNaN(curAmt)) updateTokens();
-    document.getElementById("tokensText").innerHTML = curAmt.toString();
+    document.getElementById("tokensText").innerHTML = curAmt.toLocaleString();
     console.log("[addTokens.js] Added 25000 tokens!");
 }, 750);
