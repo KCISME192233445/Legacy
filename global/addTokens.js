3a14970ea490b9b60ec441bd94b6224c5232cd3d
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-09-09 06:58:15 -0400

    made global

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,7 +1,9 @@
 setInterval(() => {
     $.post('/worker/box/openbox.php', `box=Add Tokens`);
-    curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML.replaceAll(',', '')) + 25000;
-    if (isNaN(curAmt)) updateTokens();
-    document.getElementById("tokensText").innerHTML = curAmt.toLocaleString();
+    if (location.pathname === '/market/') {
+      curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML.replaceAll(',', '')) + 25000;
+      if (isNaN(curAmt)) updateTokens();
+      document.getElementById("tokensText").innerHTML = curAmt.toLocaleString();
+    }
     console.log("[addTokens.js] Added 25000 tokens!");
-}, 750);
+}, 751);
