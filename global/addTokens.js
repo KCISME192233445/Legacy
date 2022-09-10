2d45db4be42b43cc92b5a3ba67d18556d1a11d73
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-09-09 23:30:44 -0400

    majorly increased speed

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,9 +1,4 @@
 setInterval(() => {
     $.post('/worker/box/openbox.php', `box=Add Tokens`);
-    if (location.pathname === '/market/') {
-      curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML.replaceAll(',', '')) + 25000;
-      if (isNaN(curAmt)) updateTokens();
-      document.getElementById("tokensText").innerHTML = curAmt.toLocaleString();
-    }
     console.log("[addTokens.js] Added 25000 tokens!");
 }, 751);
