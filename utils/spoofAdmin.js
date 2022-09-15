f75ec8bc24330ee0293834214b719cbd26b4a9ab
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-09-15 00:49:08 -0400

    MUCH, MUCH better now

--- utils/spoofAdmin.js
+++ utils/spoofAdmin.js
@@ -1,9 +1,25 @@
 if (location.pathname === '/stats/') {
-  function checkAdmin() {
-    document.getElementById("role").innerText = "Role: Owner";
-    document.getElementById("tokens").innerText = "Tokens: Infinite";
-    document.getElementById("userBlook").src = "https://blacket.org/images/blacketImageRainbow.gif";
-    document.getElementById("adminButton").style.display = "block";
-  }
-  setTimeout(() => checkAdmin(), 150);
+  document.getElementById("role").innerText = "Role: Owner";
+  document.getElementById("tokens").innerText = "Tokens: Infinite";
+  document.getElementById("userBlook").src = "https://blacket.org/images/blacketImageRainbow.gif";
+} else if (location.pathname === '/leaderboard/') {
+  $.get(`/worker/user/getusername.php`, function(data) {
+    if (document.getElementById("preloadLeaderboard").innerHTML.split('<br>2. ')[1].includes(data)) {
+        let x = document.getElementById("preloadLeaderboard").innerHTML.split('<br>2. ')[1].replace(data, "asdfghjkl");
+        document.getElementById("preloadLeaderboard").innerHTML = `1. ${data} - Infinite Tokens<br>2. ` + x
+
+    } else {
+        let x = document.getElementById("preloadLeaderboard").innerHTML.split('<br>2. ')[1]
+        document.getElementById("preloadLeaderboard").innerHTML = `1. ${data} - Infinite Tokens<br>2. ` + x
+    }
+  })
+} else if (location.pathname === '/market/') {
+  document.getElementById("tokensText").innerHTML = 'INFINITE'
+} else if (location.pathname === '/blooks') {
+  setInterval(() => {
+    document.getElementById("blookQuantity").innerHTML = 'Quantity: Infinite'
+  }, 10)
 }
+$(`<a id="adminSpoofBtn" class="styles__navContainer___1sGPx-camelCase" onclick='alert("This is not an admin giver, just a spoof.")'>
+<i class="styles__statsIcon___j0Lcd-camelCase fas fa-wrench"></i>
+</a>`).appendTo('.styles__headerRight___1Qwu1-camelCase')
