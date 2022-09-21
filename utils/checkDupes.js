db09e71d7edca1ce0793ee96cc8be36036ca3e95
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-09-20 22:08:43 -0400

    added check dupes

---
+++ utils/checkDupes.js
@@ -0,0 +1,8 @@
+for (let i = 0; i <= window.maxID; i++) {
+  $.get(`/worker/misc/getblook.php?id=${i}`, (res) => {
+    $.get(`/worker/blook/getuserblook.php?blook=${res.split('|')[0].replace(/\s/g, '')}`, (res2) => {
+      if (Number.parseInt(res2) === 0 || Number.parseInt(res2) === 1) return
+      console.log(res.split('|')[0].replace(/\s/g, '') + ' || ' + res2)
+    })
+  })
+}
