830157fac1a8f623bd59c9a036cc47c4cd8d08de
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-09-27 06:49:06 -0400

    minified

--- global/spamOpenBoxes.js
+++ global/spamOpenBoxes.js
@@ -1,41 +1,28 @@
-zename = prompt("Which box would you like to open?");
-i = 0;
-amt = Number.parseInt(prompt(`How many of the ${zename} Box would you like to open?`));
+let i = 0;
+const colors = {
+  Divine: 'violet',
+  Mystical: 'plum',
+  Perfect: 'lemonchiffon',
+  Chroma: 'skyblue',
+  Legendary: 'gold',
+  Epic: 'red',
+  Rare: 'blue',
+  Uncommon: 'lime'
+}
+
+let zename = prompt("Which box would you like to open?");
+let amt = Number(prompt(`How many of the ${zename} Box would you like to open?\ntype "*" to unlock all possible with your current tokens.`));
+if (isNaN(amt)) amt = Number.MAX_VALUE
 
 function buyBox(name) {
-    $.post('/worker/box/openbox.php', `box=${name}`, function(data) {
-        if (data.includes("rate")) i--;
-        blookRarity = data.split('|')[1];
-        blookUnlocked = data.split('|')[0];
-        updateTokens();
-        switch (blookRarity) {
-            case "Uncommon":
-                console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px lime;', `${blookUnlocked}`);
-                break;
-            case "Rare":
-                console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px blue;', `${blookUnlocked}`);
-                break;
-            case "Epic":
-                console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px red;', `${blookUnlocked}`);
-                break;
-            case "Legendary":
-                console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px gold;', `${blookUnlocked}`);
-                break;
-            case "Chroma":
-                console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px skyblue;', `${blookUnlocked}`);
-                break;
-            case "Perfect":
-                console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px lemonchiffon;', `${blookUnlocked}`);
-                break;
-            case "Mystical":
-                console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px plum;', `${blookUnlocked}`);
-                break;
-            case "Divine":
-                console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px violet;', `${blookUnlocked}`);
-                break;
-        }
-    });
+  $.post('/worker/box/openbox.php', `box=${name}`, function(data) {
+    if (data === 'You\'re being rate limited.') return
+    if (data.includes("rate")) i--;
+    console.log('%c%s', `color: white; font-size: 25px; text-shadow: 0px 0px 15px ${colors[data.split('|')[1]]}`, `${data.split('|')[0]}`);
+    updateTokens();
+  });
 }
+
 var check = setInterval(() => {
     if (i <= amt) {
         buyBox(zename);
