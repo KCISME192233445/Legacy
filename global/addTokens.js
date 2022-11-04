53ca0ee3033bc61bcb50f17e85b5e50dc2fbc7b4
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-11-03 21:41:03 -0400

    added semicolon

--- global/addTokens.js
+++ global/addTokens.js
@@ -4,6 +4,6 @@ setInterval(() => {
       curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML.replaceAll(',', '')) + 25000;
       if (isNaN(curAmt)) updateTokens();
       document.getElementById("tokensText").innerHTML = curAmt.toLocaleString();
-    }
+    };
     console.log("[addTokens.js] Added 2,500 tokens!");
 }, 751);
