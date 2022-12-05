cb884f6e3308819cf41bcb0a69ad25a94133a733
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-12-05 08:13:10 -0500

    here's a fix for my broken ass aids code :skull:

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,15 +1,12 @@
 if (location.pathname === '/market/') {
-    setInterval(() => {
-      $.post('/worker/box/openbox.php', `box=Add Tokens`);
-      curAmt = Number.parseInt(document.getElementById("tokensText").innerHTML.replaceAll(',', '')) + 25000;
-      if (isNaN(curAmt)) updateTokens();
-      document.getElementById("tokensText").innerHTML = curAmt.toLocaleString();
-    };
+  setInterval(() => {
+    $.post('/worker/box/openbox.php', `box=Add Tokens`);
+    updateTokens();
     console.log("[addTokens.js] Added 25,000 tokens!");
   }, 751);
 } else {
-    setInterval(() => {
-      $.post('/worker/box/openbox.php', `box=Add Tokens`);
-      console.log("[addTokens.js] Added 25,000 tokens!");
-    }, 751)
-}
+  setInterval(() => {
+    $.post('/worker/box/openbox.php', `box=Add Tokens`);
+    console.log("[addTokens.js] Added 25,000 tokens!");
+  }, 751);
+};
