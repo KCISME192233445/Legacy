7cbb5e7ac2d4999e33f451f5c2847900d6f28a2e
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-12-31 18:13:15 -0500

    fixed issues

--- global/addTokens.js
+++ global/addTokens.js
@@ -4,13 +4,13 @@ if (location.pathname === '/market/') {
       if (data === 'Blacket|Divine|/images/blacketImageRainbow.gif') {
         updateTokens();
         console.log("[addTokens.js] Added 25,000 tokens!");
-      }
-    }
+      };
+    });
   }, 200);
 } else {
   setInterval(() => {
     $.post('/worker/box/openbox.php', `box=Add Tokens`, function(data) {
       if (data === 'Blacket|Divine|/images/blacketImageRainbow.gif') console.log("[addTokens.js] Added 25,000 tokens!");
-    }
+    });
   }, 200);
 };
