854f136f21cd6be79ac3321022297e0a464d71bd
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-12-31 18:12:09 -0500

    new system, better logging

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,12 +1,16 @@
 if (location.pathname === '/market/') {
   setInterval(() => {
-    $.post('/worker/box/openbox.php', `box=Add Tokens`);
-    updateTokens();
-    console.log("[addTokens.js] Added 25,000 tokens!");
-  }, 751);
+    $.post('/worker/box/openbox.php', `box=Add Tokens`, function(data) {
+      if (data === 'Blacket|Divine|/images/blacketImageRainbow.gif') {
+        updateTokens();
+        console.log("[addTokens.js] Added 25,000 tokens!");
+      }
+    }
+  }, 200);
 } else {
   setInterval(() => {
-    $.post('/worker/box/openbox.php', `box=Add Tokens`);
-    console.log("[addTokens.js] Added 25,000 tokens!");
-  }, 751);
+    $.post('/worker/box/openbox.php', `box=Add Tokens`, function(data) {
+      if (data === 'Blacket|Divine|/images/blacketImageRainbow.gif') console.log("[addTokens.js] Added 25,000 tokens!");
+    }
+  }, 200);
 };
